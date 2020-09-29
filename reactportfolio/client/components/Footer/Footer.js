60ee28579409272de90fd8c5821dc2a9a101876c
Keiana Hanson <kiki_hanson@aol.com>
2020-09-28 20:56:27 -0400

    organized files; added footer component

---
+++ reactportfolio/client/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="footer" id="FooterStyle">
+        <h3><span> website designed by Kiki Alexis</span></h3>
+      </footer>
+    )
+};
+
+export default Footer;
