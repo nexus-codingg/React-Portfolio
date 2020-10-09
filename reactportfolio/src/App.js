8ca2c00703920d46f747f08a3db4b883c907a148
Keiana Hanson <kiki_hanson@aol.com>
2020-10-09 01:32:17 -0400

    removed bullets from navbar

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage/HomePage";
-import Container from "./components/Container/Container";
+// import Container from "./components/Container/Container";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Contact from "./pages/Contact/Contact";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
+import NavBar from "./components/NavBarAndLinks/NavBarAndLinks";
 import "./App.css";
 
 function App() {
 
   return (
     <Router>
-      <Container>
-        <Header />
+        <NavBar />
         <Route exact path="/" component={HomePage} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
-      </Container>
       <Footer />
     </Router >
   )
