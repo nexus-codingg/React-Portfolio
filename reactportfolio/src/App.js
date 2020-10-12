9deb3b474ffc74aa0d78711422cbf8015ea75a95
Keiana Hanson <kiki_hanson@aol.com>
2020-10-11 22:08:19 -0400

    troubleshooting hompage improper rendering on GitHub pages; adjusting hompepage link in package json & navlinks components ; and app.js

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -17,6 +17,7 @@ function App() {
         <NavBar />
         <Route exact path= "/React-Portfolio/" component={HomePage} />
         <Route exact path= "/Home" component={HomePage} />
+        <Route exact path= "/" component={HomePage} />
         <Route exact path="/Portfolio" component={Portfolio} />
         <Route exact path="/Contact" component={Contact} />
       <Footer />
